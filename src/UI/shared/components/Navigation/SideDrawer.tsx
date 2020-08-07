ef9dd7c15ee199233acbcfe7261228b74d29435f
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-07 23:43:28 +0530

    converted to portal

--- src/UI/shared/components/Navigation/SideDrawer.tsx
+++ src/UI/shared/components/Navigation/SideDrawer.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from './SideDrawer.module.scss';
 
 const SideDrawer: React.FC = (props) => {
-    return (
+    const content = (
         <aside className={styles['side-drawer']}>
             {props.children}
         </aside>
     );
+
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook') as HTMLElement);
 };
 
 export default SideDrawer;
