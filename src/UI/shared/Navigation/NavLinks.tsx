36ceda1022c0e56ad2ba908f1db474d595ecb3a9
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-10 07:30:30 +0530

    Listening to auth context

--- src/UI/shared/Navigation/NavLinks.tsx
+++ src/UI/shared/Navigation/NavLinks.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './NavLinks.module.scss';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../context';
+
 const NavLinks: React.FC = (props) => {
+    const auth = useContext(AuthContext);
+
     return (
         <ul className={styles['nav-links']}>
             <li>
                 <NavLink exact to="/" activeClassName={styles['active']}>ALL USERS</NavLink>
             </li>
-            <li>
-                <NavLink to="/2/places" activeClassName={styles['active']}>MY PLACES</NavLink>
-            </li>
-            <li>
-                <NavLink to="/places/new" activeClassName={styles['active']}>ADD PLACE</NavLink>
-            </li>
-            <li>
-                <NavLink to="/auth" activeClassName={styles['active']}>LOGIN</NavLink>
-            </li>
+            {
+                auth.isLoggedIn && (
+                    <li>
+                        <NavLink to="/2/places" activeClassName={styles['active']}>MY PLACES</NavLink>
+                    </li>
+                )
+            }
+            {
+                auth.isLoggedIn && (
+                    <li>
+                        <NavLink to="/places/new" activeClassName={styles['active']}>ADD PLACE</NavLink>
+                    </li>
+                )
+            }
+            {
+                !auth.isLoggedIn && (
+                    <li>
+                        <NavLink to="/auth" activeClassName={styles['active']}>LOGIN</NavLink>
+                    </li>
+                )
+            }
         </ul>
     );
 };
