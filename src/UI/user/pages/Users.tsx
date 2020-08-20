b33ed6840fd36ceb567da0afc4684c4452c5a540
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-21 04:20:50 +0530

    Fetching users

--- src/UI/user/pages/Users.tsx
+++ src/UI/user/pages/Users.tsx
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Users.module.scss';
 
-// Data
-import { USERS } from '../../../data/dummyUsers';
+// Model
+import { User } from '../../../models';
 
 // Components
 import UserList from '../components/UserList/UserList';
+import ErrorModal from '../../shared/components/ErrorModal/ErrorModal';
+import LoadingSpinner from '../../shared/components/LoadingSpinner/LoadingSpinner';
+
+interface ResponseData {
+    users: User[],
+    message?: string;
+}
 
 const Users: React.FC = () => {
+    const [users, setUsers] = useState<User[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [responseError, setResponseError] = useState<string>();
+
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch(`http://localhost:5000/users`);
+                const responseData: ResponseData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
+
+                setUsers(responseData.users);
+            } catch (err) {
+                const error = err as Error;
+                console.log(error);
+                setResponseError(error.message || `Something went wrong!`);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        sendRequest();
+    }, []);
+
+    const resetResponseErrorHandler = () => {
+        setResponseError(undefined);
+    }
+
     return (
-        <div className={styles['users-page']}>
-            <UserList
-                users={USERS}
+        <React.Fragment>
+            <ErrorModal
+                error={responseError}
+                onClear={resetResponseErrorHandler}
             />
-        </div>
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && users && (
+                <div className={styles['users-page']}>
+                    <UserList
+                        users={users}
+                    />
+                </div>
+            )}
+        </React.Fragment>
     );
 };
 
